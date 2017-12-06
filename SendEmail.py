85508ad374a2bd0d910d67d59274897f756f7389
cao-shuai <cao-shuai@github.com>
2017-12-06 20:45:36 +0800

    fixed mail information from config xml

--- SendEmail.py
+++ SendEmail.py
@@ -1,18 +1,50 @@
 import smtplib
 from email.mime.text import MIMEText
+import xml.sax
+
+
+#xml handler for parser xml
+class EmailHandler(xml.sax.ContentHandler):
+    def __init__(self):
+        self.mail_host="";
+        self.mailto_list=[];
+        self.mail_user="";
+        self.mail_pass="";
+        self.CurrentData="";
+
+    def startElement(self,tag,attributes):
+        self.CurrentData=tag;
+
+    def endElement(self,tag):
+        self.CurrentData="";
+
+    def characters(self,content):
+        if self.CurrentData == "mail_host":
+            self.mail_host=content;
+        elif self.CurrentData == "mail_user":
+            self.mail_user=content;
+        elif self.CurrentData == "mail_password":
+            self.mail_pass=content;
+        elif self.CurrentData == "perstion":
+            self.mailto_list.append(content);
 
 class Email(object):
-    mailto_list=[];
-    mail_host="smtp.163.com";
-    mail_user="mstar_scott_cao";
-    mail_pass="mstar123";#pop3 SMTP ,Customer client password,login password is mstar123;
-    mail_postfix="163.com";
-    me="<"+mail_user+"@"+mail_postfix+">";
-    
-    def add_send_to(self,author,mail_postfix="mstarsemi.com"):
-        self.mailto_postfix=mail_postfix;
-        emailaddr=author+"@"+self.mailto_postfix;
-        self.mailto_list.append(emailaddr);
+
+    def __init__(self):
+        #create XML Reader
+        parser=xml.sax.make_parser();
+        #turn off namepsaces
+        parser.setFeature(xml.sax.handler.feature_namespaces,0);
+        #rewrite ContextHanler
+        Handler=EmailHandler();
+        parser.setContentHandler(Handler);
+        parser.parse("./config/email.xml");
+
+        self.mail_host=Handler.mail_host;
+        self.mail_user=Handler.mail_user;
+        self.mail_pass=Handler.mail_pass;
+        self.mailto_list=Handler.mailto_list;
+
     
     def SendEmails(self):
         self.__send_mail__(self.mailto_list, "Mantis Overdue", "OverDue Mantis ID: XXXXX");
@@ -20,10 +52,10 @@ class Email(object):
     def __send_mail__(self,to_list,sub,content):
         msg = MIMEText(content,_subtype='plain');  
         msg['Subject'] = sub;  
-        msg['From'] = self.me;
+        msg['From'] = self.mail_user;
         msg['To'] = ";".join(to_list);
         server = smtplib.SMTP();
         server.connect(self.mail_host,25);
         server.login(self.mail_user,self.mail_pass);
-        server.sendmail(self.me, to_list, msg.as_string());
+        server.sendmail(self.mail_user, to_list, msg.as_string());
         server.close();
--- Test.py
+++ Test.py
@@ -4,9 +4,7 @@ class TestClass(object):
 
 	def TestsendEmailClass(self):
 		email=Email();
-		email.add_send_to("scott.cao");
 		email.SendEmails();
-		
 
 if __name__ == '__main__':
 	test=TestClass();
