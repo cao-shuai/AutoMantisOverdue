e8e78c4d46cc9dbeb193355d939e5d8f8532805d
cao-shuai <cao-shuai@github.com>
2017-12-08 23:25:21 +0800

    add config mail title and construct mail content file is an html file

--- base/SendEmail.py
+++ base/SendEmail.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python
+#coding: utf-8
+import os
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 import xml.sax
 
 
@@ -25,6 +29,10 @@ class EmailHandler(xml.sax.ContentHandler):
             self.mail_user=content;
         elif self.CurrentData == "mail_password":
             self.mail_pass=content;
+        elif self.CurrentData == "mail_title":
+        	self.mail_title=content;
+        elif self.CurrentData == "content-path":
+        	self.mail_content_path=content;
         elif self.CurrentData == "perstion":
             self.mailto_list.append(content);
 
@@ -44,25 +52,39 @@ class Email(object):
         self.mail_user=Handler.mail_user;
         self.mail_pass=Handler.mail_pass;
         self.mailto_list=Handler.mailto_list;
+        self.mail_title=Handler.mail_title;
+        self.mail_content_path=Handler.mail_content_path;
 
     
     def SendEmails(self):
-        if self.__send_mail__(self.mailto_list, "Mantis Overdue", "OverDue Mantis ID: XXXXX"):
+    	#construct email content!!!
+    	#test info begin
+    	try:
+    		htmlf=open(self.mail_content_path);
+	    	html = htmlf.read();
+    	finally:
+    		htmlf.close();
+        #send email 
+        if self.__send_mail__(html):
             return True;
         else:
             return False;
 
-    def __send_mail__(self,to_list,sub,content):
-        msg = MIMEText(content,_subtype='plain');  
-        msg['Subject'] = sub;  
+    def __send_mail__(self,html):
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = self.mail_title;  
         msg['From'] = self.mail_user;
-        msg['To'] = ";".join(to_list);
+        msg['To'] = ";".join(self.mailto_list);
+        part = MIMEText(html, 'html');
+        msg.attach(part);
+
         try:
             server = smtplib.SMTP();
             server.connect(self.mail_host,25);
             server.login(self.mail_user,self.mail_pass);
-            server.sendmail(self.mail_user, to_list, msg.as_string());
+            server.sendmail(self.mail_user, self.mailto_list, msg.as_string());
             server.close();
             return True;
         except (Exception):
             return False;
+    	
